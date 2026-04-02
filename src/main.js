b116ffd43297276cfe381ff94a9b785498a36ba5
extremum9 <denprogman99@gmail.com>
2026-04-02 23:36:28 +0300

    feat(events): add slider

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+import Swiper from 'swiper';
+import { Navigation, A11y } from 'swiper/modules';
+
 import initTopbar from './modules/topbar';
 
 initTopbar();
+
+new Swiper('.js-events-slider', {
+  modules: [Navigation, A11y],
+  slidesPerView: 1.98,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.js-events-slider-button-next',
+    prevEl: '.js-events-slider-button-previous'
+  }
+});
