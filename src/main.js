1e4dcd5f071a4e6c817fe460431aa0606622d8ed
extremum9 <denprogman99@gmail.com>
2026-04-03 17:58:59 +0300

    feat(events): make responsive down to 320px

--- src/main.js
+++ src/main.js
@@ -7,10 +7,23 @@ initTopbar();
 
 new Swiper('.js-events-slider', {
   modules: [Navigation, A11y],
-  slidesPerView: 1.98,
   spaceBetween: 20,
   navigation: {
     nextEl: '.js-events-slider-button-next',
     prevEl: '.js-events-slider-button-previous'
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 1.2
+    },
+    576: {
+      slidesPerView: 1.5
+    },
+    768: {
+      slidesPerView: 1.98
+    }
   }
 });
